f03ec560c9792a26606fe82bd869efe72f1da137
xX-poggers-Xx <74419801+xX-poggers-Xx@users.noreply.github.com>
2021-05-14 20:39:36 +0100

    Allows trader entities and hostile mobs to be leashed

---
+++ src/main/java/com/github/quaoz/mixin/EntityMixins.java
@@ -0,0 +1,43 @@
+package com.github.quaoz.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.Npc;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.passive.MerchantEntity;
+import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+// Allows trader entities (villagers and wandering traders) to be leashed
+@Mixin(MerchantEntity.class)
+abstract class TraderEntityMixin extends PassiveEntity implements Npc {
+    protected TraderEntityMixin(EntityType<? extends PassiveEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(method = "canBeLeashedBy", at = @At("RETURN"), cancellable = true)
+    private void onCanBeLeashedBy(CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(cir.getReturnValue() || !this.isLeashed());
+    }
+}
+
+// Allows hostile mobs to be leashed
+// There is probably a better way to do this but this is the first mod i've ever made so yeah
+@Mixin(MobEntity.class)
+abstract class MobEntityMixin extends Entity implements Npc {
+    protected MobEntityMixin(EntityType<? extends Entity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Shadow public abstract boolean isLeashed();
+
+    @Inject(method = "canBeLeashedBy", at = @At("RETURN"), cancellable = true)
+    private void onCanBeLeashedBy(CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(!this.isLeashed());
+    }
+}
