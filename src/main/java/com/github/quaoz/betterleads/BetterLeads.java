886e1eb24d108dfaebc02310f7b008aab99492b3
quaoz <quaoz@protonmail.com>
2021-05-24 11:45:05 +0100

    Add log
    Signed-off-by: quaoz <quaoz@protonmail.com>

--- src/main/java/com/github/quaoz/betterleads/BetterLeads.java
+++ src/main/java/com/github/quaoz/betterleads/BetterLeads.java
@@ -1,8 +1,12 @@
 package com.github.quaoz.betterleads;
 
 import net.fabricmc.api.ClientModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class BetterLeads implements ClientModInitializer {
+	public static final String MODID = "betterleads";
+	public final Logger logger = LogManager.getLogger(MODID);
 	private static BetterLeads INSTANCE;
 	public final BetterLeadsConfig config = new BetterLeadsConfig(this);
 
@@ -14,7 +18,18 @@ public class BetterLeads implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		INSTANCE = this;
+		this.log("uwu");
+
 		this.config.load();
 
 	}
+
+	public void log(String info) {
+		this.logger.info("[BetterLeads] " + info);
+	}
+
+	public void warn(String info) {
+		this.logger.warn("[BetterLeads] " + info);
+	}
+
 }
